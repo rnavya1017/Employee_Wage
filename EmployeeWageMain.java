44b71261a67f4840e630484915e0f215b4b936f1
R Navya <ravagunta.navya@gmail.com>
2026-06-28 17:19:30 +0530

    [Navya]:calculate monthly wage

--- EmployeeWageMain.java
+++ EmployeeWageMain.java
@@ -6,7 +6,7 @@ public class EmployeeWageMain {
 
         EmployeeWage wage = new EmployeeWage();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1. Check Attendance\n2. Calculate Daily Wage \n3. Part Time Wage \n4.Employee Wage \n5. Calculate Monthly Wage \n6.Calculate Wage for 100 Hours \n7. Exit");
+        System.out.println("1. Check Attendance\n2. Calculate Daily Wage \n3. Part Time Wage \n4.Employee Wage \n5. Calculate Monthly Wage \n 6. Exit");
         System.out.print("Enter your choice: ");
         int choice=scanner.nextInt();
 
@@ -26,10 +26,8 @@ public class EmployeeWageMain {
             case 5:
                 wage.calculateMonthlyWage();
                 break;
+            
             case 6:
-                wage.calculateWageTillCondition();
-                break;
-            case 7:
                 System.out.println("Exiting...");
                 System.exit(0);
             default:
